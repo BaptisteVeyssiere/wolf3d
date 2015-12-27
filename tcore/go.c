8141f1b06b7b0d26d5e76a449daf837e90de2272
Baptiste veyssiere <baptiste.veyssiere@epitech.eugit config --global user.email baptiste.veyssiere@epitech.eugit config --global user.name veyssi_bgit commit -am 1>
2015-12-27 21:06:49 +0100

    com

--- tcore/go.c
+++ tcore/go.c
@@ -5,10 +5,10 @@
 ** Login   <VEYSSI_B@epitech.net>
 **
 ** Started on  Mon Dec 14 20:20:56 2015 Baptiste veyssiere
-** Last update Fri Dec 25 00:37:37 2015 Baptiste veyssiere
+** Last update Sun Dec 27 21:02:16 2015 Baptiste veyssiere
 */
 
-#include "prototypes.h"
+#include <lapin.h>
 
 void	go(t_bunny_position *curpos, double angle,
 	   t_bunny_position *newpos, int move)
